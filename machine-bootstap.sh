cdaeb5c9cc6574d7dcf6d43c3c7cea10ac1cc36d
Max Gordon <gordonmaxc@gmail.com>
2017-04-15 13:40:50 -0700

    Added missing lines

--- machine-bootstap.sh
+++ machine-bootstap.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir ~/workspace
 
 # Install Homebrew
@@ -6,6 +8,7 @@ mkdir ~/workspace
 # Install zshell (oh-my-zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="superjarin"/g' ~/.zshrc
+sed -i '' 's/plugins=(git)/plugins=(git z compleat common-aliases)/g' ~/.zshrc; z
 
 # Install and configure rbenv
 brew install rbenv
@@ -14,12 +17,19 @@ rbenv install 2.4.1
 rbenv global 2.4.1
 gem install bundler
 
+# Install languages
+brew cask install java
+brew install node
+
 # Install Applications
 brew cask install iterm2
 brew cask install google-chrome
 brew cask install shiftit
 
-# Editors
+brew install Caskroom/versions/alfred2
+brew cask install alfred2
+
+# Install Editors
 brew cask install atom
 brew cask install intellij-idea-ce
 
@@ -27,5 +37,13 @@ brew cask install intellij-idea-ce
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
+# Configure Dock
+brew install dockutil
+dockutil --remove all
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock orientation -string "right"
+defaults write com.apple.dock tilesize -int 20
+killall Dock
 
 # Add global gitignore
+cp ./.gitignore ~/.gitignore
